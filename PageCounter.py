e1a1f83482d96b47ba2900da25493954f73af9f3
Bonifacio2 <bonifaciosegundo@MacBook-de-Bonifacio-Segundo.local>
2013-03-18 21:25:39 -0300

    removendo tabs hediondos

--- PageCounter.py
+++ PageCounter.py
@@ -4,37 +4,37 @@ from urllib import urlretrieve
 
 class PageCounter(HTMLParser):
 
-	def __init__(self):
-		HTMLParser.__init__(self)
-		self.parsing_pagina = False
-		self.content = ''
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.parsing_pagina = False
+        self.content = ''
 
-	def handle_starttag(self, tag, attrs):
-		if tag == 'p':
-			for attr in attrs:
-				if attr[0] == 'class' and attr[1] == 'paginaAtual':
-					self.parsing_pagina = True
+    def handle_starttag(self, tag, attrs):
+        if tag == 'p':
+            for attr in attrs:
+                if attr[0] == 'class' and attr[1] == 'paginaAtual':
+                    self.parsing_pagina = True
 
-	def handle_data(self, data):
-		if self.parsing_pagina:
-			self.content += data
+    def handle_data(self, data):
+        if self.parsing_pagina:
+            self.content += data
 
-	def handle_endtag(self, tag):
-		if self.parsing_pagina:
-			self.parsing_pagina = False
+    def handle_endtag(self, tag):
+        if self.parsing_pagina:
+            self.parsing_pagina = False
 
-	def get_page_count(self):
-		return int(self.content[self.content.rfind('/') + 1:])
+    def get_page_count(self):
+        return int(self.content[self.content.rfind('/') + 1:])
 
 
 if __name__ == '__main__':
 
-	counter = PageCounter()
+    counter = PageCounter()
 
-	url = r'http://www.portaltransparencia.gov.br/PortalTransparenciaListaAcoes.asp?Exercicio=2004&SelecaoUF=1&SiglaUF=PB&CodMun=2235&Pagina='
-	data = urlopen(url).read()
-	data = unicode(data, 'iso-8859-1')
-	counter.feed(data)
+    url = r'http://www.portaltransparencia.gov.br/PortalTransparenciaListaAcoes.asp?Exercicio=2004&SelecaoUF=1&SiglaUF=PB&CodMun=2235&Pagina='
+    data = urlopen(url).read()
+    data = unicode(data, 'iso-8859-1')
+    counter.feed(data)
 
-	print counter.get_page_count()
+    print counter.get_page_count()
 
